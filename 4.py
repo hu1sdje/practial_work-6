421edde5a9e40dea83db1a781548d0a09193a661
hu1sdje <159676546+hu1sdje@users.noreply.github.com>
2024-03-30 12:46:54 +0700

    4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,11 @@
 cell = input()
-alphabet = 'abcdefgh'
-if int(cell[1]) % 2 != 0 and cell[0] in alphabet and 0 < int(cell[1]) < 9:
-    print('черная')
+if cell[0] == 'a' or cell[0] == 'c' or cell[0] == 'e' or cell[0] == 'g':
+    if int(cell[1]) % 2 == 0:
+        print('белая')
+    else:
+        print('черная')
 else:
-    print('белая')
+    if int(cell[1]) % 2 == 0:
+        print('черная')
+    else:
+        print('белая')
